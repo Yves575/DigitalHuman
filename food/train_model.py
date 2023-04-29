75c36521492cd869cf61978307e892e66b6ad81e
Ubuntu <ubuntu@ip-172-31-25-46.eu-west-3.compute.internal>
2023-04-29 07:17:36 +0000

    update

--- food/train_model.py
+++ food/train_model.py
@@ -1,3 +1,8 @@
+import warnings
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
 from mindmeld.components.nlp import NaturalLanguageProcessor
 nlp = NaturalLanguageProcessor('.')
 nlp.build()
+print(nlp.process('I do a lot of sport'))
+print(type(nlp.process('When does Elm Street close?')))
